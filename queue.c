546bdb57ecf57412817d6cce9fcd3fd54902264b
dalaoqi <mystes3016@gmail.com>
2020-09-11 14:25:40 +0800

    Implement q_insert_tail function
    Insert a new node at the last place in queue.
    Copy the new string to new allocated memory space.
    Deal with queue is NULL or malloc fails.

--- queue.c
+++ queue.c
@@ -79,10 +79,31 @@ bool q_insert_head(queue_t *q, char *s)
  */
 bool q_insert_tail(queue_t *q, char *s)
 {
-    /* TODO: You need to write the complete code for this function */
-    /* Remember: It should operate in O(1) time */
-    /* TODO: Remove the above comment when you are about to implement. */
-    return false;
+    if (!q) {
+        return false;
+    }
+    list_ele_t *newt;
+    newt = malloc(sizeof(list_ele_t));
+    if (!newt) {
+        return false;
+    }
+    size_t length = strlen(s) + 1;
+    newt->value = (char *) malloc(length * sizeof(char));
+    if (!newt->value) {
+        free(newt);
+        return false;
+    }
+    snprintf(newt->value, length, "%s", s);
+    newt->next = NULL;
+    q->size++;
+    if (!q->tail) {
+        q->tail = newt;
+        q->head = newt;
+        return true;
+    }
+    q->tail->next = newt;
+    q->tail = newt;
+    return true;
 }
 
 /*
